e640058f3c939cd006b5ab96fee6ecdb6701c651
dabiaoji <1693094517@qq.com>
2022-06-19 21:39:21 +0800

    springmvc 1.1

--- src/com/houtianyu/controller/FirstController.java
+++ src/com/houtianyu/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.houtianyu.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@org.springframework.stereotype.Controller
 public class FirstController implements Controller {
+    @RequestMapping("/firstController")
     @Override
     public ModelAndView handleRequest(HttpServletRequest request,HttpServletResponse response){
         ModelAndView mav = new ModelAndView();
